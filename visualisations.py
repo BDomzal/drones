c73c18f3df32350cf49d2f53387abb2631a4f966
BDomzal <basiadomzal@gmail.com>
2021-06-03 12:38:23 +0200

    fix conflict

--- visualisations.py
+++ visualisations.py
@@ -1,10 +1,10 @@
 import numpy as np
 import seaborn as sns
 import random
-from matplotlib import pyplot as plt, animation
+from matplotlib import pyplot as plt, animation, patches
 
 
-def visualise(time, values, drone_radius=None, drone_height=None, heightmax=None, filename='plot'):
+def visualise(time, values, drone_radius=None, drone_height=None, heightmax=None, heightmin=None, filename='plot'):
     """
     Function produces animated simulation of the drones movement. Based on the results from simulations functions.
     :param time: vector of timestamps
@@ -14,28 +14,35 @@ def visualise(time, values, drone_radius=None, drone_height=None, heightmax=None
     frame_num, drone_num = values.shape
     drone_num //= 2
     drone_col = sns.color_palette(None, drone_num)
+    end_distance = np.max(values[-1,])+3
+
     # TODO: czy wysokosci powinny byc losowane z rozkladu jednostajnego czy normalnego
     if drone_height is None:
         drone_height = [random.uniform(1, 20) for i in range(drone_num)]
         heightmax = 25
+        heightmin = -5
     # TODO: wielkosc drona jako paramter, polaczone z funkcja wiatru
+    # changed drone shape to rectangle
     if drone_radius is None:
         drone_radius = [1 for i in range(drone_num)]
+    drone_width = 0.01*end_distance
 
-
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(10, 5))
 
     def anim_frame(k):
         plt.cla()
-        ax.set_xlim((0, np.max(values[-1,])))
-        ax.set_ylim((-5, heightmax))
+        ax.set_xlim((0, end_distance))
+        ax.set_ylim((heightmin, heightmax))
         ax.get_yaxis().set_visible(False)
-        ax.set_xlabel('Położenie')
+        ax.set_xlabel('Polozenie')
         for i in range(drone_num):
-            ax.add_patch(plt.Circle((values[k, i], drone_height[i]), drone_radius[i], color=drone_col[i]))
+            # # circle
+            # ax.add_patch(plt.Circle((values[k, i], drone_height[i]), drone_radius[i], color=drone_col[i]))
+            # rectangle
+            ax.add_patch(patches.Rectangle(xy=(values[k, i], drone_height[i]), height=drone_radius[i], width=drone_width, color=drone_col[i]))
         return ax
 
-    anim = animation.FuncAnimation(fig, func=anim_frame, frames=np.arange(0, frame_num, 1000), interval=0.02)
+    anim = animation.FuncAnimation(fig, func=anim_frame, frames=np.arange(0, frame_num, 1000), interval=0.015)
 
     Writer = animation.writers['ffmpeg']
     writer1 = Writer(fps=60, metadata={'artist': 'Me'}, bitrate=1800)
@@ -44,7 +51,56 @@ def visualise(time, values, drone_radius=None, drone_height=None, heightmax=None
     # If it had to be saved as gif
     # anim.save('pplot1.gif', writer='imagemagick')
 
+def visualise_wind(time, values, wind, timeswitch, drone_radius=None, drone_height=None, heightmax=None, heightmin=None, filename='plot'):
+    """
+    Function produces animated simulation of the drones movement. Based on the results from simulations functions.
+    :param time: vector of timestamps
+    :param values: positions of drones at the timestamps
+    :return:
+    """
+    frame_num, drone_num = values.shape
+    drone_num //= 2
+    drone_col = sns.color_palette(None, drone_num)
+    end_distance = np.max(values[-1,])+3
 
+    # TODO: czy wysokosci powinny byc losowane z rozkladu jednostajnego czy normalnego
+    if drone_height is None:
+        drone_height = [random.uniform(1, 20) for i in range(drone_num)]
+        heightmax = 25
+        heightmin = -5
+    # TODO: wielkosc drona jako paramter, polaczone z funkcja wiatru
+    # changed drone shape to rectangle
+    if drone_radius is None:
+        drone_radius = [1 for i in range(drone_num)]
+    drone_width = 0.01*end_distance
+
+    fig, ax = plt.subplots(figsize=(10, 5))
+
+    def anim_frame(k):
+        plt.cla()
+        ax.set_xlim((0, end_distance))
+        ax.set_ylim((heightmin, heightmax))
+        ax.get_yaxis().set_visible(False)
+        ax.set_xlabel('Polozenie')
+        if time[k]<timeswitch:
+            ax.set_title('H='+str(wind[0]))
+        else:
+            ax.set_title('H=' + str(wind[1]))
+        for i in range(drone_num):
+            # # circle
+            # ax.add_patch(plt.Circle((values[k, i], drone_height[i]), drone_radius[i], color=drone_col[i]))
+            # rectangle
+            ax.add_patch(patches.Rectangle(xy=(values[k, i], drone_height[i]), height=drone_radius[i], width=drone_width, color=drone_col[i]))
+        return ax
+
+    anim = animation.FuncAnimation(fig, func=anim_frame, frames=np.arange(0, frame_num, 1000), interval=0.015)
+
+    Writer = animation.writers['ffmpeg']
+    writer1 = Writer(fps=60, metadata={'artist': 'Me'}, bitrate=1800)
+    anim.save(filename+'.mp4', writer=writer1)
+
+    # If it had to be saved as gif
+    # anim.save('pplot1.gif', writer='imagemagick')
 
 
 
